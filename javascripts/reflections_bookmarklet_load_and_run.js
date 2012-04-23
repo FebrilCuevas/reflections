a32c4fc7fe3e8a5d17266153cc1bb0e1fc121299
Dominick <dguzzo@gmail.com>
2012-04-22 18:08:41 -0700

    background color on external sites set properly now. better bookmarklet timeout structure.

--- javascripts/reflections_bookmarklet_load_and_run.js
+++ javascripts/reflections_bookmarklet_load_and_run.js
@@ -1,4 +1,5 @@
 javascript: 
+var masonryAppended = false, reflectAppended = false, timeout = 250;
 console.log('appending jquery');
 var scriptElem = document.createElement('script');
 scriptElem.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
@@ -7,47 +8,57 @@ document.getElementsByTagName('head')[0].appendChild(scriptElem);
 (function ensureJQueryLoaded() {
 	if (typeof jQuery === "function") {
 		
-		console.log('appending masonry plugin');
-		scriptElem = document.createElement('script');
-		scriptElem.src = 'http://www.telecommutetojuryduty.com/scripts/external/jquery.masonry.min.js';
-		document.getElementsByTagName('head')[0].appendChild(scriptElem);
-		console.log('appending reflections stylesheet');
-		var styleElem = document.createElement('link');
-		styleElem.href = 'http://www.telecommutetojuryduty.com/misc/reflections/stylesheets/reflections.css';
-		styleElem.rel = 'stylesheet';
-		styleElem.type = 'text/css';
-		document.getElementsByTagName('head')[0].appendChild(styleElem);
-		 
-		(function ensureMasonryLoaded() {
-			console.log('appending reflections');
+		if ( !masonryAppended ){
+			console.log('appending masonry plugin');
 			scriptElem = document.createElement('script');
-			scriptElem.src = 'https://raw.github.com/dguzzo/reflections/master/javascripts/jquery.reflections.js';
+			scriptElem.src = 'http://www.telecommutetojuryduty.com/scripts/external/jquery.masonry.min.js';
 			document.getElementsByTagName('head')[0].appendChild(scriptElem);
+			console.log('appending reflections stylesheet');
+			var styleElem = document.createElement('link');
+			styleElem.href = 'http://www.telecommutetojuryduty.com/misc/reflections/stylesheets/reflections.css';
+			styleElem.rel = 'stylesheet';
+			styleElem.type = 'text/css';
+			document.getElementsByTagName('head')[0].appendChild(styleElem);
+			masonryAppended = true;
+		}
+		
+		(function ensureMasonryLoaded() {
 			
 		    if (typeof jQuery.fn.masonry === 'function') {
-		        (function ensureReflectLoaded() {
-							if ( typeof jQuery.fn.reflectImages === 'function' ) {
-								jQuery('img').reflectImages({
-									'rotation': 45,
-									'opacity': 0.4,
-									'stripAllButImages': true,
-									'removeAnimatedGifs': true,
-									'ensureMinNumberOfImages' : 20
-								});
-							}
-							else{
-								setTimeout(function() {
-									ensureReflectLoaded();
-								},
-								200);
-							}
+			
+					if( !reflectAppended ){
+						console.log('appending reflections');
+						scriptElem = document.createElement('script');
+						scriptElem.src = 'https://raw.github.com/dguzzo/reflections/master/javascripts/jquery.reflections.js';
+						document.getElementsByTagName('head')[0].appendChild(scriptElem);
+						reflectAppended = true;
+					}
+				
+	        (function ensureReflectLoaded() {
+
+						if ( typeof jQuery.fn.reflectImages === 'function' ) {
+							jQuery('img').reflectImages({
+								'rotation': 45,
+								'opacity': 0.4,
+								'stripAllButImages': true,
+								'removeAnimatedGifs': true,
+								'ensureMinNumberOfImages' : 20
+							});
+						}
+						else{
+							setTimeout(function() {
+								ensureReflectLoaded();
+							},
+							timeout);
+						}
 					})();
 		    }
 				else {
 					setTimeout(function() {
+						console.log('ensureMasonryLoaded timeout');
 						ensureMasonryLoaded();
 					},
-					200);
+					timeout);
 		    }
 		})();
 	}
@@ -55,7 +66,7 @@ document.getElementsByTagName('head')[0].appendChild(scriptElem);
 		setTimeout(function() {
     	ensureJQueryLoaded();
     },
-    200);
+    timeout);
 	}
 })();
 
